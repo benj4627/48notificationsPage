a921f210fe9b0ef8c3f03786354a955cd3fc1186
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-04-04 08:48:40 +0200

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let redDotEl = document.querySelectorAll('.dot');
 let notificationEl = document.querySelectorAll('.notification');
 
 //Looper igennem notifications med forEach. Tilføjer eventListener og ændrer styling. Derudover tilføjer hidden class til de røde prikker, og ændret antallet af nye notifikation til 0. 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener('click', () => { 
     notificationEl.forEach(notifications => { 
         notifications.style.backgroundColor = 'white';
         notifications.style.boxShadow = 'unset';
@@ -15,16 +15,21 @@ buttonEl.addEventListener('click', () => {
         redDots.classList.add('hidden');
     });
     numberEl.textContent = '0';
-    numberEl.style.backgroundColor ='hsl(219, 14%, 63%)';
 });
 
-//Her er målet at tildele "læst" styling til individuelle notifikationer, og samtidig fjerne den røde prik når der bliver trykket.  
-notificationEl.forEach(notificationsClick => {
+//Her er målet at tildele "læst" styling til individuelle notifikationer. 
+document.querySelectorAll('.notification').forEach(notificationsClick => {
     notificationsClick.addEventListener('click', () =>{
         notificationsClick.style.backgroundColor = 'white';
         notificationsClick.style.boxShadow = 'unset';
         notificationsClick.classList.add('padding');
         let redDotEl = notificationsClick.querySelector('.dot');
         redDotEl.classList.add('hidden');
+
+        // Opdatere antallet af ulæste notifikationer
+        //'.notification:not(.padding) vælger alle elementer med klassen "notification", der ikke har klassen "padding" (de ulæste notifikationer).
+        //Derefter hentes antallet elementer via  length, som repræsenterer antallet af ulæste notifikationer.
+            let unreadNotificationsCount = document.querySelectorAll('.notification:not(.padding)').length;
+            numberEl.textContent = unreadNotificationsCount;
     });
 });
